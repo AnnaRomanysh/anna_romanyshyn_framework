1f6334617f871a50a9c41a9e5f41878bde718c2a
AnnaRomanysh@gmail.com <you@example.com>
2018-02-13 10:05:19 +0200

    edit

---
+++ src/main/java/ui/element/IElemet.java
@@ -0,0 +1,8 @@
+package ui.element;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.internal.Locatable;
+import org.openqa.selenium.internal.WrapsElement;
+
+public interface IElemet extends WebElement, WrapsElement, Locatable {
+}
